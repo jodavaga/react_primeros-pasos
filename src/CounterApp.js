a02c51cebe0b3f97ee33eb0cbf29c4eba28721d3
Jose D. Vasquez <jodavaga@gmail.com>
2020-07-04 14:46:44 -0500

    create and set counter state using hooks

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -3,7 +3,11 @@ import PropTypes from 'prop-types';
 
 const CounterApp = ({value}) => {
 
+<<<<<<< HEAD
     const [counter, setCounter] = useState(10);
+=======
+    const [counter, setCounter] = useState(value);
+>>>>>>> 0025e75... create and set counter state using hooks
 
     // click event
     const handleAdd = () => {
