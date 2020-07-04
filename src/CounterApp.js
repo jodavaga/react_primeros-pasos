0c18d27866144110a6ead4f58fc4299a71b37a9c
Jose D. Vasquez <jodavaga@gmail.com>
2020-07-04 14:08:22 -0500

    handle event click

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -2,10 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({value}) => {
+
+    // click event
+    const handleAdd = (event) => {
+        event.persist();
+        
+        console.log(event);
+        const evetType = event.type;
+
+        // Event Pooling - https://es.reactjs.org/docs/events.html#event-pooling
+        setTimeout(() => {
+            console.warn(evetType);
+            console.log(event);
+        }, 1000);
+    }
+
     return (
         <>
             <h1>Counter App</h1>
             <h4> {value} </h4>
+
+            {/* <button onClick={ (e) => handleAdd(e) }> +1 </button> */}
+            <button onClick={ handleAdd }> +1 </button>
         </>
     )
 }
