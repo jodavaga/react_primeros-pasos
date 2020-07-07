5db903bc93f33f52376be5197de9eb91977bfd99
Jose D. Vasquez <jodavaga@gmail.com>
2020-07-07 11:46:08 -0500

    Enzyme config. test on <PrimeraApp /> using enzyme and enzyme-to-json

--- src/tests/PrimeraApp.test.js
+++ src/tests/PrimeraApp.test.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { shallow } from 'enzyme';
+// import { render } from '@testing-library/react';
 import PrimeraApp from '../PrimeraApp';
 
 describe('Primera App tests', () => {
 
-    test('Should display saludo: "Hola, soy jose"', () => {
+    // test('Should display saludo: "Hola, soy jose"', () => {
+    //     const mensaje = 'Hola, soy jose';
 
+    //     const {getByText} = render( <PrimeraApp saludo={ mensaje } /> );
+
+    //     expect(getByText(mensaje)).toBeInTheDocument();
+    // });
+
+    test('Should render <PrimeraApp /> correctly', () => {
+        
         const mensaje = 'Hola, soy jose';
+        const wrapper = shallow( <PrimeraApp saludo={mensaje} /> )
 
-        const {getByText} = render( <PrimeraApp saludo={ mensaje } /> );
-
-        expect(getByText(mensaje)).toBeInTheDocument();
-
-    })
+        expect( wrapper ).toMatchSnapshot();
+    });
+    
 })
